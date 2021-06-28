b0dd9788c33839ab8170318956acd343da18427b
SanketTambe39 <sankettambe39@gmail.com>
2021-06-28 20:19:02 +0530

    ADDED: class method with function parameter

--- EmployeeWageComputation/src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ EmployeeWageComputation/src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -7,6 +7,8 @@ public class EmployeeWageComputation {
 		EmployeeWageCalculate employeeWageCalculate = new EmployeeWageCalculate();
 		employeeWageCalculate.calculateWage(20, 40, 8, 4, "Bridgelabz");
 		employeeWageCalculate.calculateWage(30, 50, 9, 4, "EC-Mobility");
+		employeeWageCalculate.calculateWage(24, 30, 7, 4, "Mphasis");
+		employeeWageCalculate.calculateWage(28, 60, 5, 2, "Four Piller");
 	}
 	
 }
