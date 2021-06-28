0d0bd9774fe0a44b6bf1efca802f2b27346d50b8
SanketTambe39 <sankettambe39@gmail.com>
2021-06-28 18:55:33 +0530

    ADDED: Employees is Present or not checked

--- EmployeeWageComputation/src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ EmployeeWageComputation/src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -2,9 +2,20 @@ package com.bridgelabz.employeewagecomputation;
 
 public class EmployeeWageComputation {
 
+	public static final int isPresent = 1;
+	
 	public static void main(String[] args) {
-		
-		System.out.println("Welcome To Employee Wage Computation Problem");
+		  
+        double empCheck = Math.floor(Math.random() * 10 ) % 2;
+        
+        if (empCheck == isPresent)
+        {
+        	System.out.println("Employee is Present");
+        }
+        else
+        {
+        	System.out.println("Employee is Absent");
+        }
 
 	}
 
