440162a0d6ff948d58f7d417c0570d9cfbc854cf
SanketTambe39 <sankettambe39@gmail.com>
2021-06-28 19:33:59 +0530

    ADDED: Employee Wage of month Calculated

--- EmployeeWageComputation/src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ EmployeeWageComputation/src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -2,33 +2,36 @@ package com.bridgelabz.employeewagecomputation;
 
 public class EmployeeWageComputation {
 
-	public static final int IS_FULL_TIME = 1;
-	public static final int IS_PART_TIME = 2;
-	public static final int PART_TIME = 4;
-	public static final int FULL_DAY = 8;
-	public static final int WAGE_PER_HOUR = 20;
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int WORKING_DAYS=20;
+	
+	public static void main(String[] args) {
+
+		int WAGE_PER_HOUR=20;
+		int EMP_HRs=0;
+		int DAILY_WAGE=0;
+		
+		for (int i = 0; i < WORKING_DAYS; i++) 
+		{
+			double empCheck=Math.floor(Math.random() * 10) % 3;
+	
+			switch ((int) empCheck) {
+			case IS_FULL_TIME:
+					EMP_HRs=8;
+					break;
+			case IS_PART_TIME:
+					EMP_HRs=4;
+					break;
+			default:
+					EMP_HRs=0;
+			}
 			
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to Employee Wage Computation" );
-        int dailywage= 0;
-        double empCheck = Math.floor(Math.random() * 10 ) % 3;
-        
-        if (empCheck == IS_FULL_TIME)
-        {
-        	dailywage = FULL_DAY * WAGE_PER_HOUR;    
-         	System.out.println("Employee is Present for Full Time ");
-         	System.out.println("Daily wage is :"+ dailywage);
-        }
-        else if ( empCheck == IS_PART_TIME )
-        {
-        	dailywage = PART_TIME * WAGE_PER_HOUR;    
-         	System.out.println("Employee is Present for Part Time");
-         	System.out.println("Daily wage is :"+ dailywage);
-        }
-        else
-        {
-        	System.out.println("Employee is Absent");
-        }
-    }
+				if(empCheck>=1) 
+					DAILY_WAGE=DAILY_WAGE+WAGE_PER_HOUR*EMP_HRs;
+		}
+		
+		System.out.println("Total Wage of an employee for a month ::"+DAILY_WAGE);
+
+	}
 }
