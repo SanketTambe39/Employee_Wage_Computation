bff97d30088fb4611d637cf2e33b3d9448962933
SanketTambe39 <sankettambe39@gmail.com>
2021-06-28 19:37:14 +0530

    ADDED: Limitations on working month and working hours

--- EmployeeWageComputation/src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ EmployeeWageComputation/src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -2,18 +2,21 @@ package com.bridgelabz.employeewagecomputation;
 
 public class EmployeeWageComputation {
 
+
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
-	public static final int WORKING_DAYS=20;
+	
 	
 	public static void main(String[] args) {
-
+		int WORKING_DAYS=0;
 		int WAGE_PER_HOUR=20;
 		int EMP_HRs=0;
-		int DAILY_WAGE=0;
+		int TOTAL_WAGE=0;
+		int TOTAL_HRs=0;
 		
-		for (int i = 0; i < WORKING_DAYS; i++) 
+		while( WORKING_DAYS <20 && TOTAL_HRs<100   ) 
 		{
+			WORKING_DAYS++;
 			double empCheck=Math.floor(Math.random() * 10) % 3;
 	
 			switch ((int) empCheck) {
@@ -26,12 +29,13 @@ public class EmployeeWageComputation {
 			default:
 					EMP_HRs=0;
 			}
-			
-				if(empCheck>=1) 
-					DAILY_WAGE=DAILY_WAGE+WAGE_PER_HOUR*EMP_HRs;
+
+			TOTAL_HRs+=EMP_HRs;
+			if(empCheck>=1) 
+				TOTAL_WAGE=TOTAL_WAGE+WAGE_PER_HOUR*EMP_HRs;
 		}
 		
-		System.out.println("Total Wage of an employee for a month ::"+DAILY_WAGE);
+		System.out.println("Employee got Wage :: "+TOTAL_WAGE+" by working "+TOTAL_HRs+" HRs & "+WORKING_DAYS+" Days");
 
 	}
 }
